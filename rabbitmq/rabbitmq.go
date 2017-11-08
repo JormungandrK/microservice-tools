12f751d1265d81137591fcd76c8239d6ea19d9a6
Vladimir Tomanovski <vladoohr@gmail.com>
2017-11-08 15:07:15 +0100

    Test rabbitmq functions

---
+++ rabbitmq/rabbitmq.go
@@ -0,0 +1,66 @@
+package rabbitmq
+
+import (
+	"github.com/streadway/amqp"
+)
+
+// Publish sends body to an exchange on the server.
+func Publish(name string, body []byte, channel *amqp.Channel) error {
+	q, err := channel.QueueDeclare(
+		name,  // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	err = channel.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         body,
+		})
+	return err
+}
+
+func Consume(name string, channel *amqp.Channel) (<-chan amqp.Delivery, error) {
+	q, err := channel.QueueDeclare(
+		name,  // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		return nil, nil
+	}
+
+	err = channel.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	if err != nil {
+		return nil, nil
+	}
+
+	msgs, err := channel.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	return msgs, err
+}
