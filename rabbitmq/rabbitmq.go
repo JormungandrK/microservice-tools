d8486fa31784cfb6dd8f75453925c0e3444ed86d
Vladimir Tomanovski <vladoohr@gmail.com>
2017-11-08 16:17:43 +0100

    Test connection to the RabbitMQ

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -4,6 +4,24 @@ import (
 	"github.com/streadway/amqp"
 )
 
+// Dial returns a new Connection over TCP using PlainAuth
+func Dial(username, password, host, port string) (*amqp.Connection, *amqp.Channel, error) {
+	conn, err := amqp.Dial(
+		fmt.Sprintf("amqp://%s:%s@%s:%s/",
+			username,
+			password,
+			host,
+			port,
+		),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ch, err := conn.Channel()
+	return conn, ch, err
+}
+
 // Publish sends body to an exchange on the server.
 func Publish(name string, body []byte, channel *amqp.Channel) error {
 	q, err := channel.QueueDeclare(
@@ -31,6 +49,7 @@ func Publish(name string, body []byte, channel *amqp.Channel) error {
 	return err
 }
 
+// Consume immediately starts delivering queued messages.
 func Consume(name string, channel *amqp.Channel) (<-chan amqp.Delivery, error) {
 	q, err := channel.QueueDeclare(
 		name,  // name
